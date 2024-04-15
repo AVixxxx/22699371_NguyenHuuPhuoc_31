65b967f34ce4592f4fbab21c4811cc18a9103f78
AVixxxx <phuochuu1092004@gmail.com>
2024-04-15 12:19:50 +0700

    add project

---
+++ js/check-out.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#checkOut").click(function() {
+        // Gán giá trị "false" cho key "status" trong localStorage
+        localStorage.setItem('status', JSON.stringify(false));
+        // Chuyển hướng trình duyệt đến trang "homePage.html"
+        window.location.href = "homePage.html";
+    });
+
+    // Lấy thông tin người dùng từ localStorage
+    let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+    // Hiển thị thông tin người dùng trong các phần tử HTML
+    $("#username").html(userInfo.name);
+    $("#email").html(userInfo.email);
+    $("#address").html(userInfo.address);
+    $("#phone").html(userInfo.phone);
+});
